dff0f7c3cc43e39ea409422650caf4e5914edc48
AronQ1234 <aronalen2@gmail.com>
2024-07-27 19:10:22 +0200

    fixing error

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -7,7 +7,7 @@ const Upcoming = () => {
       <h1 className='text-3xl font-bold'>
         Upcoming
       </h1>
-      <CallList type="upcomung"/>
+      <CallList type="upcoming"/>
     </section>
   )
 }
